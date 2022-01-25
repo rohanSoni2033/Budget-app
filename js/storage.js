19f8aafef56a1faed9414e30250f48a0d5832cae
rohan soni <rohansoni54476@gmail.com>
2022-01-25 18:04:12 +0530

    Complete Storage Class

--- js/storage.js
+++ js/storage.js
@@ -1,17 +1,54 @@
 export default class Storage {
-  constructor(username, id) {
-    this.username = username;
-    this.id = id;
-  }
-  _getAllData() {
-    const expensesData = localStorage.getItem('expensesData');
-
-    return JSON.parse(expensesData);
+  static _getAllData() {
+    const expensesList = localStorage.getItem('expensesList');
+    return JSON.parse(expensesList);
   }
 
-  _saveData() {}
+  static _saveData(data) {
+    const savedExpensesList = this._getAllData();
 
-  _deleteData() {}
+    savedExpensesList[0].transactions.push({
+      id: Math.floor(Math.random() * 10000000000),
+      date: new Date().toLocaleDateString(),
+      name: data.name,
+      type: data.type,
+      amount: data.amount,
+    });
 
-  _updateData() {}
+    this._saveToLocalStrorage(savedExpensesList);
+  }
+
+  static _deleteData(id) {
+    const savedExpensesList = this._getAllData();
+    const index = savedExpensesList[0].transactions.findIndex(
+      (t) => transaction.id === id
+    );
+
+    if (index > -1) {
+      savedExpensesList[0].transactions.splice(index, 1);
+      this._saveToLocalStrorage(savedExpensesList);
+    }
+  }
+
+  static _updateData(id, data) {
+    const savedExpensesList = this._getAllData();
+
+    const index = savedExpensesList[0].transactions.findIndex(
+      (t) => t.id === id
+    );
+
+    const transaction = savedExpensesList[0].transactions[index];
+
+    if (index > -1) {
+      transaction.name = data.name;
+      transaction.type = data.type;
+      transaction.amount = data.amount;
+    }
+
+    this._saveToLocalStrorage(savedExpensesList);
+  }
+
+  _saveToLocalStrorage(savedExpensesList) {
+    localStorage.setItem('expensesList', JSON.stringify(savedExpensesList));
+  }
 }
